a67f9713dd4c40b2bd37899de40489be9ac4245b
andrewgodman <andrew@godman.io>
2018-12-07 08:15:37 +1100

    added build page

---
+++ site/precache-manifest.436ad6f70ca5d1ac15156d10ececa3d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce8d7c41a590c9da8a4a",
+    "url": "/static/js/main.ce8d7c41.chunk.js"
+  },
+  {
+    "revision": "87826c592231199e20db",
+    "url": "/static/js/1.87826c59.chunk.js"
+  },
+  {
+    "revision": "ce8d7c41a590c9da8a4a",
+    "url": "/static/css/main.d4db7bd4.chunk.css"
+  },
+  {
+    "revision": "bc7f8fcc76fa0f5830fcc9fd9965f7d4",
+    "url": "/index.html"
+  }
+];
